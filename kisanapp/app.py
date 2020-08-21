caac1ba5032f9565678563ac656d6494b29ae151
Rabin Kalikote <rabinkalikote33@gmail.com>
2020-08-21 10:57:03 +0545

    Scrapping also with beautiful soup

--- kisanapp/app.py
+++ kisanapp/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return 'Yay! You are on Flask.'
+@app.route('/api')
+def api():
+    url = 'https://techverse.info/askmattrab-new-milestone-achieved-for-online-learning-in-nepal'
+    d = {}
+    content = requests.get(url).content
+    soup = BeautifulSoup(content, 'html.parser')
+    d['title'] = soup.find('h1', {'class': 'single-title'}).text
+    return d
